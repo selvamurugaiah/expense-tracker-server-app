d0c5e7ca6bf32cf964b09ca30180e53c8a70e794
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-11-03 12:44:30 +0530

    final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dbConnect = require('./config/dbConnect');
 const userRoute = require('./routes/users/usersRoute');
 const incomeRoute = require('./routes/income/incomeRoute');
@@ -9,6 +10,7 @@ const expenseRoute = require('./routes/expenses/expenseRoute');
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 dbConnect()
 
